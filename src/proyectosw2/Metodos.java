0e1239ed57387f23c99b8a2d047cff347c6f0db8
Administrator <Administrator@sistemas04.estudiantes.uco>
2014-08-01 20:12:27 -0500

    sss

---
+++ src/proyectosw2/Metodos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosw2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Metodos {
+
+        public void realizar_pago(String tipo_de_pago) {
+            
+        }
+    
+}
